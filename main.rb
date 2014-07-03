4f2c4efcde6aaa33e393ad1573391a9c572e09ab
tracymu <tracy.musung@moomumedia.com>
2014-07-03 18:39:05 +1000

    Created squares_next_moves hash

--- main.rb
+++ main.rb
@@ -27,34 +27,29 @@ end
 
 
 def next_squares(square, board)
-  # Have to put in both the square so I know where it is, and the board
-  # so I know where the limits of the board are
-  
-  possible_moves = []
-  
+  possible_moves = [] 
   x_moves = [2,-2,2,-2,-1,1,-1,1]
   y_moves = [1,-1,-1,1,2,2,-2,-2]
   
   for i in (0..7) 
     next_x = square.x + x_moves[i]
-    next_y = square.y + y_moves[i]
-    
+    next_y = square.y + y_moves[i]    
     if next_x.between?(0,board.max_x) && next_y.between?(0,board.max_y)
       possible_moves << Square.new(next_x,next_y)
-    end
-    
+    end   
   end
   
   possible_moves.map! {|square| square.chess_name}
-  
-  return possible_moves 
-
 end
 
 new_board = Board.new(8,8)
 
- puts next_squares(new_board.squares[62], new_board)
+# puts next_squares(new_board.squares[62], new_board)
 
+squares_next_moves = {}
 
+for square in new_board.squares
+  squares_next_moves[square] = next_squares(square,new_board)
+end
 
-
+puts squares_next_moves
