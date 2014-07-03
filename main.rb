30c897bda1a386f68408d7fecec2942fde714e71
tracymu <tracy.musung@moomumedia.com>
2014-07-03 19:54:11 +1000

    created find_options method to search the hash for the next moves for any square

--- main.rb
+++ main.rb
@@ -1,55 +1,39 @@
-class Square
-  attr_accessor :x, :y, :chess_name
-  # Make a square at position x,y
-  def initialize(x, y)
-    @x = x
-    @y = y
-    @chess_name = "#{(x+65).chr}#{y+1}"
-  end
-  
-end
-
-class Board
-  attr_accessor :squares, :max_x, :max_y
-  
-  def initialize(rows, columns)
-    @squares = []
-    for i in 0..(columns-1)
-      for j in 0..(rows-1)
-        @squares << Square.new(i,j)
-      end
-    end
-    
-    @max_x = rows-1
-    @max_y = columns-1
-  end
-end
-
+require_relative 'square'
+require_relative 'board'
 
 def next_squares(square, board)
   possible_moves = [] 
   x_moves = [2,-2,2,-2,-1,1,-1,1]
   y_moves = [1,-1,-1,1,2,2,-2,-2]
-  
   for i in (0..7) 
     next_x = square.x + x_moves[i]
     next_y = square.y + y_moves[i]    
     if next_x.between?(0,board.max_x) && next_y.between?(0,board.max_y)
       possible_moves << Square.new(next_x,next_y)
     end   
-  end
-  
+  end    
   possible_moves.map! {|square| square.chess_name}
+end
+
+def squares_next_moves(board)
+  @next_moves = {} 
+  for square in board.squares
+    @next_moves[square] = next_squares(square,board)
+  end
+  return @next_moves
+end
+
+def find_options(board, start_input)
+  @start_input = start_input
+  next_moves = squares_next_moves(board)
+  next_moves.each_key do |square| 
+    if start_input == square.chess_name
+      return next_moves[square]
+    end
+  end
 end
 
 new_board = Board.new(8,8)
 
-# puts next_squares(new_board.squares[62], new_board)
+next_squares = find_options(new_board,'A3')
 
-squares_next_moves = {}
-
-for square in new_board.squares
-  squares_next_moves[square] = next_squares(square,new_board)
-end
-
-puts squares_next_moves
