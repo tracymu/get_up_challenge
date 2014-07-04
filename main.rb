2c22198a6fffab05d1a9710d6edd1c5db11af2dc
tracymu <tracy.musung@moomumedia.com>
2014-07-05 08:55:43 +1000

    Refactored possible moves methods into one. Started build_paths method.

--- main.rb
+++ main.rb
@@ -1,39 +1,55 @@
 require_relative 'square'
 require_relative 'board'
 
-def next_squares(square, board)
-  possible_moves = [] 
+def squares_possible_moves(board)
+  possible_moves = {}
   x_moves = [2,-2,2,-2,-1,1,-1,1]
   y_moves = [1,-1,-1,1,2,2,-2,-2]
-  for i in (0..7) 
-    next_x = square.x + x_moves[i]
-    next_y = square.y + y_moves[i]    
-    if next_x.between?(0,board.max_x) && next_y.between?(0,board.max_y)
-      possible_moves << Square.new(next_x,next_y)
-    end   
-  end    
-  possible_moves.map! {|square| square.chess_name}
-end
-
-def squares_next_moves(board)
-  @next_moves = {} 
   for square in board.squares
-    @next_moves[square] = next_squares(square,board)
-  end
-  return @next_moves
+    possible_moves[square] = []
+    for i in (0..7) 
+      next_x = square.x + x_moves[i]
+      next_y = square.y + y_moves[i]
+      if next_x.between?(0,board.max_x) && next_y.between?(0,board.max_y)
+        possible_moves[square] << Square.new(next_x,next_y)
+      end   
+    end
+  end    
+  possible_moves
 end
 
 def find_options(board, start_input)
-  @start_input = start_input
-  next_moves = squares_next_moves(board)
+  next_moves = squares_possible_moves(board)
   next_moves.each_key do |square| 
     if start_input == square.chess_name
-      return next_moves[square]
+       options = next_moves[square]
     end
+    # options.map! {|square| square.chess_name}
+    return options
   end
 end
 
-new_board = Board.new(8,8)
+def build_paths(board, start_square, end_square)
+  all_paths = []
+  options = find_options(board, start_square)
+  for i in options
+    path =[]
+    if path.include? i or i.chess_name == end_square
+      all_paths << path
+    else
+      # build_paths(board, i.chess_name, end_square)
+      path << i
+    end
+  end
+  # return all_paths
+end
 
-next_squares = find_options(new_board,'A3')
+
+new_board = Board.new(8,8)
+start_square = 'A1'
+end_square = 'C6'
+puts build_paths(new_board, start_square, end_square)
+
+
+
 
